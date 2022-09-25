2535a0f3482483905818358c49f48265f098030c
virgilleonel <virgiltako@gmail.com>
2022-09-25 22:50:52 +0200

    @Input with undefined variable

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -7,7 +7,5 @@ import { Component, } from '@angular/core';
   inputs:['ValueChild']
 })
 export class ParentComponent {
-   variableParent:string='je viens du parent'
-
-
+   variableParent:string='je viens du parent';
 }
